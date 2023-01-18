f5897d5e4f0eb2d606e983f613f52efde98bdbfb
nicozapatacruz <nicozapatacruz@gmail.com>
2023-01-18 11:30:05 -0500

    Add search results text and fix quotation space

--- app/components/PostType.tsx
+++ app/components/PostType.tsx
@@ -14,7 +14,7 @@ export default function PostType({ data, postType, likedPosts, setLikedPosts }) 
       ) : (
         <div className="w-full text-2xl text-center pt-24">
           <h1 className="pt-4">
-            We have no post of type &ldquo; {postType.charAt(0).toUpperCase() + postType.slice(1).replace("_", " ")} &rdquo; at the moment
+            We have no post of type &ldquo;{postType.charAt(0).toUpperCase() + postType.slice(1).replace("_", " ")}&rdquo; at the moment
           </h1>
           <i className="fa-light fa-face-sad-sweat text-8xl pt-10"></i>
         </div>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -18,16 +18,21 @@ export default function SearchPage() {
     <>
       <Navbar />
       {searchData.length ? (
-        <div className="grid grid-cols-4 items-center justify-between gap-5 w-full pt-12">
-          {searchData.map((data) => (
-            <div key={data.id}>
-              <PostBox data={data} likedPosts={likedPosts} setLikedPosts={setLikedPosts} />
-            </div>
-          ))}
-        </div>
+        <>
+          <div className="w-full text-2xl text-center pt-8">
+            <h1>Results for posts in &ldquo;{search}&rdquo;</h1>
+          </div>
+          <div className="grid grid-cols-4 items-center justify-between gap-5 w-full pt-8">
+            {searchData.map((data) => (
+              <div key={data.id}>
+                <PostBox data={data} likedPosts={likedPosts} setLikedPosts={setLikedPosts} />
+              </div>
+            ))}
+          </div>
+        </>
       ) : (
         <div className="w-full text-2xl text-center pt-24">
-          <h1>No posts found with the search &ldquo; {search} &rdquo;</h1>
+          <h1>No posts found with the search &ldquo;{search}&rdquo;</h1>
           <i className="fa-light fa-shop-slash text-8xl pt-10"></i>
         </div>
       )}
