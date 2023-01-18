9f0d5ac21210a36c7750743a5d57398c853b184f
nicozapatacruz <nicozapatacruz@gmail.com>
2023-01-18 09:53:25 -0500

    Make postType a global state

--- app/Context/store.tsx
+++ app/Context/store.tsx
@@ -2,22 +2,27 @@
 import { createContext, useContext, Dispatch, SetStateAction, useState } from "react";
 
 type DataType = {
-  id: number;
+  firstName: string;
 };
 
 interface ContextProps {
+  postType: string;
+  setPostType: Dispatch<SetStateAction<string>>;
   likedPosts: DataType[];
   setLikedPosts: Dispatch<SetStateAction<DataType[]>>;
 }
 
 const GlobalContext = createContext<ContextProps>({
+  postType: "",
+  setPostType: (): string => "",
   likedPosts: [],
   setLikedPosts: (): DataType[] => [],
 });
 
 export const GlobalContextProvider = ({ children }) => {
+  const [postType, setPostType] = useState("cabins");
   const [likedPosts, setLikedPosts] = useState<[] | DataType[]>([]);
-  return <GlobalContext.Provider value={{ likedPosts, setLikedPosts }}>{children}</GlobalContext.Provider>;
+  return <GlobalContext.Provider value={{ postType, setPostType, likedPosts, setLikedPosts }}>{children}</GlobalContext.Provider>;
 };
 
 export const useGlobalContext = () => useContext(GlobalContext);
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@
 import Navbar from "@/app/components/Navbar";
 import TypesNavbar from "@/app/components/TypesNavbar";
 import PostType from "@/app/components/PostType";
-import { useState } from "react";
 import { useGlobalContext } from "./Context/store";
 
 import data from "./db/data";
 
 export default function Home() {
-  const { likedPosts, setLikedPosts } = useGlobalContext();
-  const [postType, setPostType] = useState("cabins");
+  const { postType, setPostType, likedPosts, setLikedPosts } = useGlobalContext();
+  //const [postType, setPostType] = useState("cabins");
 
   return (
     <div>
