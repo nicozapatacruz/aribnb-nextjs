7db700bd5e265adcee025bbb67a5c279b6fb311c
nicozapatacruz <nicozapatacruz@gmail.com>
2023-01-22 18:16:36 -0500

    Delete GlobalContext

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./styles/globals.css";
 import "../public/fontawesome/css/all.css";
 import { Inter } from "@next/font/google";
-import { GlobalContextProvider } from "./Context/store";
 
 const font = Inter({
   weight: ["400", "700"],
@@ -15,9 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={font.className}>
-        <GlobalContextProvider>{children}</GlobalContextProvider>
-      </body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
